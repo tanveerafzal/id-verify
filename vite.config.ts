a30e197d62ed4ccdd8651dde4985a00b1343b723
Tanveer Afzal <tanveerafzal@gmail.com>
2025-12-15 18:53:42 -0500

    eqeqw

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     allowedHosts: [
       'localhost',
       '127.0.0.1',
-      'id-verify-api-960587958424.northamerica-northeast2.run.app'
+      'id-verify-api-960587958424.northamerica-northeast2.run.app',
       'f2bf1c7ea3e0.ngrok-free.app'
     ],
     proxy: {
